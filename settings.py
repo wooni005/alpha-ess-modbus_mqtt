316962dd924ccf376095bc5ee839aaa5975cb9c8
Arjan Wooning <arjan@wooning.cz>
2021-04-14 13:31:02 +0200

    Changed indent

--- settings.py
+++ settings.py
@@ -14,7 +14,7 @@ MQTT_TOPIC_CONTROL   = 'huis/AlphaEss/+/control'
 MQTT_TOPIC_CHECK     = "huis/AlphaEss/RPiInfra/check"
 MQTT_TOPIC_REPORT    = "huis/AlphaEss/RPiInfra/report"
 
-SEND_INVERTER_MSG_TIMER = 50        #5sec  [100ms]
-SEND_METER_MSG_TIMER = 300          #30sec [100ms]
-SEND_BATTERY_MSG_TIMER = 300        #30sec [100ms]
+SEND_INVERTER_MSG_TIMER      = 50   #5sec  [100ms]
+SEND_METER_MSG_TIMER         = 300  #30sec [100ms]
+SEND_BATTERY_MSG_TIMER       = 300  #30sec [100ms]
 SEND_INVERTER_TEMP_MSG_TIMER = 3000 #5min  [100ms]
